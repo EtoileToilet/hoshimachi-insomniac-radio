8abd270afd60a5cb87c8ae6257de96df6b28691c
EtoileToilet <105792733+EtoileToilet@users.noreply.github.com>
2022-06-08 18:51:37 +0700

    added color codes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express()
 
 if (webserver) {
   app.get('/', (req, res) => {
-    res.send('pong')
+    res.send('wake me up inside')
   })
 
   app.listen(port)
@@ -30,13 +30,51 @@ client.on('ready', () => {
         { filter: 'audioonly' },
       );
       stream.on('info', (info) => {
-        var songTitle = info.videoDetails.title
-        console.log(songTitle)
+        console.log(info.videoDetails.title)
+        var who = info.videoDetails.author.name
+        var channelavatar = (info.videoDetails.author.thumbnails[0].url)
+        var big = channelavatar.replace('s48-', '')
+        var min = Math.floor(info.videoDetails.lengthSeconds / 60)
+        var sec = info.videoDetails.lengthSeconds % 60
+        var color = "#000000"
+        if (who.includes("Mori") == true) {
+          color = "#C90D40"
+        } else if (who.includes("Kiara") == true) {
+          color = "#FF511C"
+        } else if (who.includes("Ina'nis") == true) {
+          color = "#62567E"
+        } else if (who.includes("Gura") == true) {
+          color = "#5D81C7"
+        } else if (who.includes("Amelia") == true) {
+          color = "#F8DB92"
+        } else if (who.includes("Sana") == true) {
+          color = "#F2D7C4"
+        } else if (who.includes("Fauna") == true) {
+          color = "#A4E5CF"
+        } else if (who.includes("Kronii") == true) {
+          color = "#0869EC"
+        } else if (who.includes("Mumei") == true) {
+          color = "#998274"
+        } else if (who.includes("Baelz") == true) {
+          color = "#D2251E"
+        } else if (who.includes("IRyS") == true) {
+          color = "#3C0024"
+        } else if (who.includes("Watame") == true) {
+          color = "#F6ECA5"
+        } //special guest 1
+          else if (who.includes("Rikka") == true) {
+          color = "#EAB3B8"
+        } //special guest 2
+        else {
+          color = "#FFFFFF"
+        }
         const embedtest = new MessageEmbed()
+          .setColor(color)
           .setTitle(`${info.videoDetails.title}`)
           .setURL(thelink)
-          .setAuthor(info.videoDetails.author.name, info.videoDetails.author.thumbnails[0].url, info.videoDetails.author.channel_url)
-          .setThumbnail(info.videoDetails.author.thumbnails[0].url)
+          .setAuthor(info.videoDetails.author.name, big, info.videoDetails.author.channel_url)
+          .setThumbnail(big)
+          .addField("Duration", `${min} min ${sec} sec`)
           .setImage(`https://i.ytimg.com/vi/${info.videoDetails.videoId}/maxresdefault.jpg`)
           .setFooter ('HoloEN Insomniac Radio', 'https://cdn.donmai.us/original/28/97/__mori_calliope_and_death_sensei_hololive_and_1_more_drawn_by_hurybone__2897434bbc538556a039edfb23ee74ab.jpg');
         client.channels.cache.get(text_id).send(`:arrow_forward: Now playing: **${info.videoDetails.title}**`, {embed: embedtest });
